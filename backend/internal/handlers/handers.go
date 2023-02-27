a4ef0fdf79d696fffac8776b60a837aa72b7c0ff
teamcutter <ruslan.sibgatulin05@gmail.com>
2023-02-27 21:20:25 +0600

    some issues

--- backend/internal/handlers/handers.go
+++ backend/internal/handlers/handers.go
@@ -1,23 +1,20 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/teamcutter/vidToAud/backend/internal/utils"
 )
 
 // ! ocRN6Hz3LrI test link !
-// ! req == 0 -> video
-// ! req == 1 -> audio
-// ! req == 2 -> both
 func GetVideoAndAudio(c *gin.Context) {
 	videoID := c.Param("videoID")
 	fileName := utils.DownloadVideo(videoID)
 	utils.ExtractAudio(fileName)
 
-	c.IndentedJSON(http.StatusOK, gin.H{
+	c.FileAttachment("/backend/static/downloaded_audio/"+fileName+".mp3", fileName+".mp3")
+	/* c.IndentedJSON(http.StatusOK, gin.H{
 		"audio": "backend/static/downloaded_audio/" + fileName + ".mp3",
 		"video": "backend/static/downloaded_video/" + fileName + ".mp4",
-	})
+		"filename": fileName,
+	}) */
 }

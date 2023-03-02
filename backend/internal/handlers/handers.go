a46844269c549f408731095950fdc80cf74446ee
ruslan <ruslan.sibgatulin05@gmail.com>
2023-03-02 20:47:51 +0600

    added client

--- backend/internal/handlers/handers.go
+++ backend/internal/handlers/handers.go
@@ -12,13 +12,8 @@ func GetAudioFromVideo(c *gin.Context) {
 	videoURL := c.Param("videoURL")
 	fileName := utils.DownloadVideo(videoURL)
 	utils.ExtractAudio(fileName)
-
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"audio":    "./static/downloaded_audio/" + fileName + ".mp3",
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"audio":    "/backend/static/downloaded_audio/" + fileName + ".mp3",
 		"filename": fileName,
 	})
-	/* c.IndentedJSON(http.StatusOK, gin.H{
-		"audio":    "backend/static/downloaded_audio/" + fileName + ".mp3",
-		"filename": fileName,
-	}) */
 }
